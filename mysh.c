0d5fc6211132c48a245e3ef2ced7a8bbfa461dc8
Katie Macalintal <kmacalintal@middlebury.edu>
2022-11-10 14:11:06 -0500

    fixed mysh with simple functionality of calling program

--- mysh.c
+++ mysh.c
@@ -7,64 +7,57 @@
 #define INPUT_MAX_LENGTH 4096
 #define MAX_NUMBER_ARGS 10
 
-char *print_prompt();
-char **read_input(char *user_input);
-void do_the_thing(char *input_args[]);
+char **get_user_input();
+void run_program(char *input_args[]);
 
 int main(int argc, char *argv[])
 {
-    char *user_input;
     char **input_args;
 
     for(;;)
     {
-        user_input = print_prompt();
-        input_args = read_input(user_input);
-        do_the_thing(input_args);
+        input_args = get_user_input();
+        run_program(input_args);
     }
     return 0;
 }
 
-char *print_prompt()
+char **get_user_input()
 {
-    char *user_input;
-    
-    //THINK ABOUT WHERE TO FREE
-    user_input = malloc(INPUT_MAX_LENGTH);
-    
+    char original_user_input[INPUT_MAX_LENGTH];
+    char *edited_user_input; 
+    static char *input_args[MAX_NUMBER_ARGS];
+
     printf("$ ");
-    fgets(user_input, INPUT_MAX_LENGTH, stdin);
+    fgets(original_user_input, INPUT_MAX_LENGTH, stdin);
+    edited_user_input = strtok(original_user_input, "\n");
 
-    return(user_input);
-    
-}
+    // ASSUME ONLY GIVE ONE ARGUMENT
+    //trailing spaces on exit? 
+    input_args[0] = strtok(edited_user_input, " "); 
 
-char **read_input(char *user_input)
-{
-    char **input_args;
-    
-    //THINK ABOUT WHERE TO FREE
-    input_args = malloc(MAX_NUMBER_ARGS);
-
-    input_args[0] = user_input;
-
-    //RUN STRTOK and at the end add a null character to the array
-    input_args[1] = NULL;
+    if(strcmp(input_args[0],"exit") == 0)
+    {
+        exit(0);
+    }
 
     return input_args;
-    //REMEMBER EXIT CASES
 }
 
-void do_the_thing(char *input_args[])
+void run_program(char *input_args[])
 {
+    int exit_value;
     //process is the child
     if(fork() == 0)
     {
-        execv(input_args[0], input_args);
+        if(execvp(input_args[0], input_args) == -1)
+        {
+            perror("evecvp"); 
+        }
     }
     //process is the parent
     else
     {
-        wait(NULL);
+        wait(&exit_value);
     }
 }
