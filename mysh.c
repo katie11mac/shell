323ad996c45f281fa1c0fbb600c6e7b5ead262ae
Katie Macalintal <kmacalintal@middlebury.edu>
2022-11-10 14:21:01 -0500

    handle more than one argument

--- mysh.c
+++ mysh.c
@@ -19,22 +19,30 @@ int main(int argc, char *argv[])
         input_args = get_user_input();
         run_program(input_args);
     }
+
     return 0;
 }
 
 char **get_user_input()
 {
     char original_user_input[INPUT_MAX_LENGTH];
-    char *edited_user_input; 
+    char *edited_user_input;
     static char *input_args[MAX_NUMBER_ARGS];
+    int counter; 
 
     printf("$ ");
     fgets(original_user_input, INPUT_MAX_LENGTH, stdin);
     edited_user_input = strtok(original_user_input, "\n");
 
+    counter = 0; 
     // ASSUME ONLY GIVE ONE ARGUMENT
     //trailing spaces on exit? 
-    input_args[0] = strtok(edited_user_input, " "); 
+    input_args[counter] = strtok(edited_user_input, " "); 
+    counter += 1; 
+    while((input_args[counter] = strtok(NULL, " ")) != NULL)
+    {
+        counter += 1; 
+    }
 
     if(strcmp(input_args[0],"exit") == 0)
     {
