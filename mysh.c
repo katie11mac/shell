9e7b711fe48ba743343d2e934533f6e87a835372
Katie Macalintal <kmacalintal@middlebury.edu>
2022-11-13 12:36:28 -0500

    implemented input redirection and output redirection; working on piping

--- mysh.c
+++ mysh.c
@@ -13,16 +13,13 @@
 
 char **get_user_input();
 void parse_user_input(char *input_args[]); 
-void run_program(char *input_args[]);
 
 int main(int argc, char *argv[])
 {
-    char **input_args;
 
     for(;;)
     {
-        input_args = get_user_input();
-        parse_user_input(input_args);
+        get_user_input();
     }
 
     return 0;
@@ -33,26 +30,42 @@ char **get_user_input()
     char original_user_input[INPUT_MAX_LENGTH];
     char *edited_user_input;
     static char *input_args[MAX_NUMBER_ARGS];
-    int counter; 
+    int arg_index; // index of the argument in the array
+
+    char *pipe_args;
+    int pipe_fds[2];
+
+    pipe_fds[0] = 0;
+    pipe_fds[1] = 1;
 
     printf("$ ");
     fgets(original_user_input, INPUT_MAX_LENGTH, stdin);
     edited_user_input = strtok(original_user_input, "\n");
 
-    counter = 0; 
-    // ASSUME ONLY GIVE ONE ARGUMENT
-    //trailing spaces on exit? 
-    input_args[counter] = strtok(edited_user_input, " "); 
-    counter += 1; 
-    while((input_args[counter] = strtok(NULL, " ")) != NULL)
-    {
-        counter += 1; 
-    }
-
-    if(strcmp(input_args[0],"exit") == 0)
+    if(strcmp(edited_user_input, "exit") == 0)
     {
         exit(0);
     }
+
+    pipe_args = strtok(edited_user_input, "|");
+
+    while(pipe_args != NULL)
+    {
+        arg_index = 0; 
+        input_args[arg_index] = strtok(pipe_args, " "); 
+        arg_index += 1; 
+        while((input_args[arg_index] = strtok(NULL, " ")) != NULL)
+        {
+            arg_index += 1; 
+        }
+        pipe(pipe_fds);
+        parse_user_input(input_args); 
+
+        pipe_args = strtok(NULL, "|"); // increment step
+    }
+
+    // strtok on | 
+    // go through 
 
     return input_args;
 }
@@ -62,6 +75,7 @@ void parse_user_input(char *input_args[])
     int i, j;
     int fd;
     int is_redirect;
+    int exit_value;
     static char *prev_args[MAX_NUMBER_ARGS];
 
     is_redirect = 0;
@@ -70,32 +84,47 @@ void parse_user_input(char *input_args[])
     while(input_args[i] != NULL && is_redirect == 0)
     {
         
-        if(strcmp(input_args[i], ">") == 0) //or >>
+        // OUTPUT REDIRECTION
+        if((strcmp(input_args[i], ">") == 0) || (strcmp(input_args[i], ">>") == 0)) //or >>
         {
             // put a >> check here so dont truncate
-            fd = open(input_args[i+1], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+            if(strcmp(input_args[i], ">") == 0)
+            {
+                fd = open(input_args[i+1], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+            }
+            else
+            {
+                fd = open(input_args[i+1], O_CREAT | O_WRONLY | O_APPEND, 0666);
+            }
             
-            //MAKE THIS ITS OWN FUNCTION WITH DESIRED FD
             if(fd == -1)
             {
                 perror("open");
             }
 
-            if(dup2(fd, 1) == -1)
-            {
-                perror("dup2");
-            }
-
+            // Process all arguments until the redirection
             for(j = 0; j < i; j++)
             {
                 prev_args[j] = input_args[j];
             }
 
-            run_program(prev_args);
-
-            if(dup2(1, fd) == -1)
+            //process is the child
+            if(fork() == 0)
             {
-                perror("dup2");
+                if(dup2(fd, 1) == -1)
+                {
+                    perror("dup2");
+                }
+
+                if(execvp(prev_args[0], prev_args) == -1)
+                {
+                    perror("evecvp");
+                }
+            }
+            //process is the parent
+            else
+            {
+                wait(&exit_value);
             }
             
             close(fd);
@@ -105,31 +134,65 @@ void parse_user_input(char *input_args[])
         }
         //if <, open file.txt (RDONLY, no mode), change fd to zero, set and send run_program prev_args
 
+        // INPUT REDIRECTION
+        if(strcmp(input_args[i], "<") == 0)
+        {
+            
+            fd = open(input_args[i+1], O_RDONLY);
+            
+            if(fd == -1)
+            {
+                perror("open");
+            }
+
+            // Process all arguments until the redirection
+            for(j = 0; j < i; j++)
+            {
+                prev_args[j] = input_args[j];
+            }
+
+            //process is the child
+            if(fork() == 0)
+            {
+                if(dup2(fd, 0) == -1)
+                {
+                    perror("dup2");
+                }
+
+                if(execvp(prev_args[0], prev_args) == -1)
+                {
+                    perror("evecvp");
+                }
+            }
+            //process is the parent
+            else
+            {
+                wait(&exit_value);
+            }
+            
+            close(fd);
+
+            is_redirect = 1;
+        }
         i++;
     }
 
     if(is_redirect == 0)
     {
-        run_program(input_args);
+        //process is the child
+        if(fork() == 0)
+        {
+            if(execvp(input_args[0], input_args) == -1)
+            {
+                perror("evecvp");
+            }
+        }
+        //process is the parent
+        else
+        {
+            wait(&exit_value);
+        }
     }
     
 
-}
-
-void run_program(char *input_args[])
-{
-    int exit_value;
-    //process is the child
-    if(fork() == 0)
-    {
-        if(execvp(input_args[0], input_args) == -1)
-        {
-            perror("evecvp");
-        }
-    }
-    //process is the parent
-    else
-    {
-        wait(&exit_value);
-    }
 }
